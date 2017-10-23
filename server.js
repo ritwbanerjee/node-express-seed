4ec915b9b9bb2513b472da3737af55af7cb18951
Banerjee, Ritwik (US - Bengaluru) <ritwbanerjee@deloitte.com>
2017-10-23 19:31:14 +0530

    change port num

--- server.js
+++ server.js
@@ -4,6 +4,5 @@ const app = express();
 
 require('./server/express.js')(app);
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(4000, function () {
 })
